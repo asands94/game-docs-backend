b575cb60bb5b1bd46b339ad3cf771909e449a051
asands94 <ashley.sands01@gmail.com>
2023-02-13 00:31:16 -0500

    add crud for game routes

--- app/routes/ideaRoutes.js
+++ app/routes/ideaRoutes.js
@@ -19,81 +19,86 @@ const router = express.Router()
 // ROUTES
 
 // POST -> create an idea(and give that idea to a game)
-// anybody should be able to give a game an idea
-// so we wont requireToken
 // our idea schema, has some non-required fields, so let's use removeBlanks
-router.post('/ideas/:gameId', removeBlanks, (req, res, next) => {
-    // isolate our idea from the request, and save to variable
+router.post('/ideas/:gameId', requireToken, removeBlanks, (req, res, next) => {
+  // isolate our idea from the request, and save to variable
   const idea = req.body.idea
-    // isolate and save our gaes's id for easy reference
-    const gameId = req.params.gameId
-    // find the game and push the new idea into the game's array
-    Game.findById(gameId)
-        // first step is to use our custom 404 middleware
-        .then(handle404)
-        // handle adding idea to game
-        .then(game => {
-            console.log('the game: ', game)
-            console.log('the idea: ', idea)
-            // add idea to idea array
-            game.ideas.push(idea)
+  // isolate and save our games's id for easy reference
+  const gameId = req.params.gameId
+  // find the game and push the new idea into the game's array
+  Game.findById(gameId)
+    // first step is to use our custom 404 middleware
+    .then(handle404)
+    // handle adding idea to game
+    .then((game) => {
+      console.log('the game: ', game)
+      console.log('the idea: ', idea)
+      // only the owner of the game should be allowed to add an idea to it
+      requireOwnership(req, game)
+      // add idea to idea array
+      game.ideas.push(idea)
 
-            // save the game
-            return game.save()
-        })
-        // send info after updating the game
-        .then(game => res.status(201).json({ game: game }))
-        // pass errors along to our error handler
-        .catch(next)
+      // save the game
+      return game.save()
+    })
+    // send info after updating the game
+    .then((game) => res.status(201).json({ game: game }))
+    // pass errors along to our error handler
+    .catch(next)
 })
 
 // PATCH -> update an idea
-router.patch('/ideas/:gameId/:ideaId', requireToken, removeBlanks, (req, res, next) => {
+router.patch(
+  '/ideas/:gameId/:ideaId',
+  requireToken,
+  removeBlanks,
+  (req, res, next) => {
     // get and save the id's to variables
     const gameId = req.params.gameId
     const ideaId = req.params.ideaId
 
     // find our game
     Game.findById(gameId)
-        .then(handle404)
-        .then(game => {
-            // single out the idea
-            const theIdea = game.ideas.id(ideaId)
-            // make sure the user is the games's owner
-            requireOwnership(req, game)
-            // update accordingly
-            theIdea.set(req.body.idea)
+      .then(handle404)
+      .then((game) => {
+        // single out the idea
+        const theIdea = game.ideas.id(ideaId)
+        // make sure the user is the games's owner
+        requireOwnership(req, game)
+        // update accordingly
+        theIdea.set(req.body.idea)
 
-            return game.save()
-        })
-        // send a status
-        .then(() => res.sendStatus(204))
-        .catch(next)
-})
+        return game.save()
+      })
+      // send a status
+      .then(() => res.sendStatus(204))
+      .catch(next)
+  }
+)
 
 // DELETE -> destroy an idea
 router.delete('/ideas/:gameId/:ideaId', requireToken, (req, res, next) => {
   const gameId = req.params.gameId
   const ideaId = req.params.ideaId
 
-    // find the game
-    Game.findById(gameId)
-        .then(handle404)
-        // grab the specific idea using its id
-        .then(game => {
-            // isolate the idea
-            const theIdea = game.ideas.id(ideaId)
-            // make sure the user is the owner of the game
-            requireOwnership(req, game)
-            // call remove on our idea subdoc
-            theIdea.remove()
-            // return the saved game
-            return game.save()
-        })
-        // send a response
-        .then(() => res.sendStatus(204))
-        // pass errors to our error handler (using next)
-        .catch(next)
+  // find the game
+  Game.findById(gameId)
+    .then(handle404)
+    // grab the specific idea using its id
+    .then((game) => {
+      // isolate the idea
+      const theIdea = game.ideas.id(ideaId)
+      // make sure the user is the owner of the game
+      requireOwnership(req, game)
+      // call remove on our idea subdoc
+      theIdea.remove()
+      // return the saved game
+      return game.save()
+    })
+    // send a response
+    .then(() => res.sendStatus(204))
+    // pass errors to our error handler (using next)
+    .catch(next)
 })
 
 // export our router
